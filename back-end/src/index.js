c6bba664c05b0826bced58cc72d71ed4fc320ef4
Romullo Bernardo <romullobernardo@XRT25-22B.local>
2019-09-04 12:23:31 -0400

    express

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,15 +1,17 @@
-import { ApolloServer } from 'apollo-server'
+import connect from 'connect'
+import { ApolloServer } from 'apollo-server-express'
+import query from 'qs-middleware'
 
 import typeDefs from './typeDefs'
 import resolvers from './resolvers'
 
+const server = new ApolloServer({ typeDefs, resolvers })
 
-const server = new ApolloServer(
-{
-    typeDefs,
-    resolvers
-})
+const app = connect()
+const path = '/graphql'
 
-server
-    .listen({ port: 4001 })
-    .then(({ url }) => console.log(`Server listening on ${url}`))
+app.use(query())
+server.applyMiddleware({ app, path })
+
+
+app.listen({ port: 4001 }, () => console.log(`Server listening on 4001`))
