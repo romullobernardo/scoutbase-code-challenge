91b86c0bb642965610c71681803d75281def7323
Romullo Bernardo <romullobernardo@XRT25-22B.local>
2019-09-08 12:56:06 -0400

    refactoring

--- back-end/src/utils/getUser.js
+++ back-end/src/utils/getUser.js
@@ -4,14 +4,9 @@ export default (req, SECRET) =>
 {
     const header = req.headers.authentication
 
-    console.log('HEADER: ', header)
-
     if (!header) throw new Error('Authentication required')
 
     const decoded = verify(header, SECRET)
-
-    console.log('DECODED: ', decoded)
-    console.log('USER ID: ', decoded.user.id)
 
     return decoded.user.id
 }
